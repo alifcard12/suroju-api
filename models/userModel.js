917ede061468b542982b0ad1b33e2736e478f546
alifcard12 <136218515+alifcard12@users.noreply.github.com>
2023-07-07 10:22:46 +0700

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -21,7 +21,7 @@ const userSchema = new mongoose.Schema(
     role: {
       type: String,
       enum: ["Admin", "Free", "Bronze", "Diamond", "Premium"],
-      default: "Pree",
+      default: "Free",
     },
     profilePict: {
       type: String,
